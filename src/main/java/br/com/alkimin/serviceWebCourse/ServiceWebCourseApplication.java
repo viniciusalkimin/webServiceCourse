b9dc9144f107947bccdd82cf782c9071e827ab1f
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2022-10-16 19:57:34 -0300

    creating base of project

---
+++ src/main/java/br/com/alkimin/serviceWebCourse/ServiceWebCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.alkimin.serviceWebCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWebCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWebCourseApplication.class, args);
+	}
+
+}
